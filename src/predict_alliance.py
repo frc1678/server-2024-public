b8baa6bbb0c2ae6b371af236e53e44481709e65a
liamSy418 <143149425+liamSy418@users.noreply.github.com>
2023-11-15 20:04:05 -0800

    New script "predict_alliance.py" and tests (#1098)
    * New predict_alliance script (prints predicted alliance data given three teams)
    
    * REFORMATTED FILES predict_alliance.py AND test_predict_alliance.py
    
    ---------
    
    Co-authored-by: root <root@SyFamLaptop>

---
+++ src/predict_alliance.py
@@ -0,0 +1,47 @@
+from calculations.predicted_aim import *
+import server
+from unittest.mock import patch
+
+# takes in _______ and returns/prints predicted scores for the alliance using functions from predicted_aim.py
+# 1. take in three teams
+# 2. find obj_team data for all three
+# 3. use predicted_aim.py functions to find predicted values for an ALLIANCE with the three teams (somehow)
+# 4. return a dictionary with predicted_aim values
+# 5. print into terminal
+
+# team numbers are strings
+def predict_alliance(team1, team2, team3, server, obj_team_data, tba_team_data):
+    output = {}
+    team_numbers = [team1, team2, team3]
+    calc = PredictedAimCalc(server)
+    predicted_values = PredictedAimScores()
+    # obj_team_data is all the obj team data in server
+    output["predicted_score"] = calc.calculate_predicted_alliance_score(
+        predicted_values, obj_team_data, tba_team_data, team_numbers
+    )
+    output["predicted_grid_score"] = calc.calculate_predicted_alliance_grid_score(predicted_values)
+    output["predicted_auto_score"] = calc.calculate_predicted_alliance_auto_score(predicted_values)
+    output["predicted_charge_score"] = calc.calculate_predicted_alliance_charge_score(
+        predicted_values
+    )
+    output["predicted_tele_score"] = calc.calculate_predicted_alliance_tele_score(predicted_values)
+    return output
+
+
+def main():
+    with patch("server.Server.ask_calc_all_data", return_value=False):
+        server1 = server.Server()
+    print(
+        predict_alliance(
+            input("team 1: "),
+            input("team 2: "),
+            input("team 3: "),
+            server1,
+            server1.db.find("obj_team"),
+            server1.db.find("tba_team"),
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
