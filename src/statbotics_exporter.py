f693de303045297dcc30cef6b18086dd1a6f6211
alisonKYoung <75290000+alisonKYoung@users.noreply.github.com>
2024-02-14 20:19:36 -0800

    Create a really ugly csv for a competition (#1414)
    * Make it work
    
    * Commented better

--- src/statbotics_exporter.py
+++ src/statbotics_exporter.py
@@ -32,6 +32,7 @@ def build_matches(comp_key):
 
 
 def build_teams(comp_key):
+    # Statbotics naturally doesn't put in some team data
     teams = sb.sb_get_team_events(event=comp_key)
     # Specify structure of data by team
     data_by_team: Dict[str, Dict[str, any]] = {}
@@ -48,7 +49,7 @@ def build_teams(comp_key):
                 column_headers.append(key)
             data_by_team[team_num][key] = value
     # Sort column_headers by team number first
-    column_headers = BaseExport.order_headers(column_headers, ["team"])
+    column_headers = BaseExport.order_headers(column_headers, ["team_name", "team"])
     return column_headers, data_by_team
 
 
@@ -81,11 +82,18 @@ def build_all_events():
 
 
 def write_data(directory_path: str, comp_key, type):
-    # Specify team or match
-    if type.lower() == "match":
+    # Specify team, match, or both
+    # Both will be a really ugly csv
+    if type.lower() == "both":
         column_headers, built_data = build_matches(comp_key)
+        team_headers, team_data = build_teams(comp_key)
+        # Combine the headers and data
+        column_headers = column_headers + team_headers
+        built_data = {**built_data, **team_data}
     elif type.lower() == "team":
         column_headers, built_data = build_teams(comp_key)
+    elif type.lower() == "match":
+        column_headers, built_data = build_matches(comp_key)
     else:
         column_headers, built_data = build_all_events()
     # Write file name
@@ -104,7 +112,7 @@ def write_data(directory_path: str, comp_key, type):
 if __name__ == "__main__":
     key = input("Enter a competition key or 'all' for all events: ")
     if key.lower() != "all":
-        team_or_match = input("What type of csv? team/match: ")
+        team_or_match = input("What type of csv? team/match/both: ")
     else:
         team_or_match = "all"
     # Currently, csv shows up in server but the file path can be changed
